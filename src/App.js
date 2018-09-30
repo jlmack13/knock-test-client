eb015efadf5946e3f9f21417f2a6446829671c98
jlmack13 <jennimacklin@gmail.com>
2018-09-30 18:42:36 -0400

    basic set up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {entriesReceived: ""}
+    this.getEntries = this.getEntries.bind(this)
+  }
+  getEntries() {
+    this.setState({entriesReceived: "Are there any entries?"})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React/Rails API Knock Demo</h1>
+          <h1 className="App-title">React/Rails API Knock Demo</h1>
         </header>
         <p className="App-intro">
           This app will be a diary app. Users can create simple entries, with just a title and content.
@@ -23,6 +32,10 @@ class App extends Component {
           <input name="password" id="password" type="password" />
         </form>
         <button onClick={this.login}>Login</button>
+
+        <button onClick={this.getEntries} style={{marginTop: '25vh'}}>Get Entries</button>
+        <h2>Entries: </h2>
+        <p>{this.state.entriesReceived}</p>
       </div>
     );
   }
